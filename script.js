783ba0062d99dc2009f037d767547a75b3dd3ecf
Pedro Wrublak <pedrovili.w@gmail.com>
2025-02-20 17:50:10 -0300

    Adicionada funcionalidade para mostrar imagem e testo na tela

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ const inputUpload = document.getElementById("image-upload");
 uploadBtn.addEventListener('click', () => {
     inputUpload.click();
 })
+
+function lerConteudoDoarquivo(arquivo) {
+    return new Promise ((resolve, reject) => {
+        const reader = new FileReader();
+        leitor.onload = () => {
+            resolve({url: reader.result, nome: arquivo.name})
+        }
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
+    })
+}
+
+const imagemPrincipal = document.querySelector("main-image");
+const nomeDaImagem = document.querySelector("container-imagem-name p");
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if(arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoarquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome
+        } catch(erro){
+            console.error("Erro na leitura do aqruivo")
+        }
+    }
+})
